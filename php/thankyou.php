015d5cbd724e7dcd52eec4ec659288d4d2caeb2a
Mariya Lazarova <67010799+MrsMariyaLazarova@users.noreply.github.com>
2023-07-23 18:45:54 +0300

    Create thankyou.php

---
+++ php/thankyou.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lagunamoon - Activated Charcoal Natural Teeth Whitening Powder with Bamboo Brush </title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" href="../images/favicon.ico">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<style>  
+        * {
+            box-sizing: border-box;
+            font-size: 17px;
+        }
+        @media screen and (max-width: 1490px) {
+            * {
+            font-size: 16px;
+            }
+        }
+        @media screen and (max-width: 450px) {
+            * {
+            font-size: 15px;
+            }
+        }
+        body {
+            font-weight: 400;
+            line-height: 1.5rem;
+            letter-spacing: 0.1px;
+            margin: auto;
+            padding: auto;
+            background-color: rgba(255,255,255,0.7);
+        }
+        .navbar {
+            display: flex;
+            align-items: center;
+            justify-content: center
+        }
+        .container-fluid {
+            background-color:rgb(132 210 194);
+            padding: 15px 0;
+        }
+        .container {
+            display: flex;
+            flex-direction: column;
+            margin-top: 50px;
+        }
+        .container h2 {
+            text-align: center;
+        }
+        @media screen and (max-width: 635px) {
+            .container h2{
+                font-size: 25px;
+            }
+        }
+        @media screen and (max-width: 485px) {
+            .container h2{
+                font-size: 20px;
+            }
+        }
+        @media screen and (max-width: 350px) {
+            .container h2{
+                font-size: 15px;
+            }
+            .container .image {
+                width: 70px;
+            }
+        }
+        .container .image {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        @media screen and (max-width: 520px) {
+            .container-fluid .logo {
+                width: 150px;
+            }
+        }
+        @media screen and (max-width: 310px) {
+            .container-fluid .logo {
+                width: 120px;
+            }
+        }
+        @media screen and (max-width: 250px) {
+            .container .image {
+                width: 150px;
+            }
+            .container-fluid .logo {
+                width: 100px;
+            }
+        }
+        .wrapper .details {
+            font-weight: 900;
+        }
+        .wrapper p {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+             width: 50%;
+            text-align: center;
+        }
+
+        @media screen and (max-width: 1163px) {
+            .wrapper p {
+                margin: auto;
+                width: 50%;
+            }
+        }
+        @media screen and (max-width: 1080px) {
+            .wrapper p {
+                font-size: 14px;
+                line-height: 1.2rem;
+            }
+            .horizontal-line {
+                margin: 50px auto;
+            }
+        }
+
+
+    </style>
+
+<div class="container-fluid">
+        <nav class="navbar navbar-expand-md navbar-light">
+            <div class="navbar-brand"><img class="logo" src="../images/logo.png" alt="" width="200px;"></div>
+        </nav>
+    </div>
+
+        <div class="container">
+            <img class="image" src="../images/img-1.jpg" alt="" width="200px;">
+            <h2>Lagunamoon - Activated Charcoal Natural<br /> Teeth Whitening Powder with Bamboo Brush</h2>
+        </div>
+
+        <hr class="horizontal-line">
+
+        <div class="wrapper">
+            <p>Thank you for your order, it was successfully received!</p>
+            <p class="details">Order details:</p>
+            <p>Order number: #365547</p>
+            <p>Selected payment method: Cash on delivery</p>
+            <p>Delivery ETA 3 to 5 business days</p>
+        </div>
+  
+  </body>
+  </html>
